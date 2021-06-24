27a631f6f301d9d76987ac25fb0d8e3aff877322
BeGifted <1124095061@qq.com>
2021-06-24 23:28:15 +0800

    中国象棋1.0

--- js/common.js
+++ js/common.js
@@ -121,8 +121,9 @@ window.onload = function(){
 	// 	com.gambit=data.split(" ");
 	// 	AI.historyBill = com.gambit;
 	// })
-	com.getData("js/store.js",
+	com.getData("js/store.json",
 		function(data){
+		console.log("读取store");
 		com.store=data.split(" ");
 	})
 }
